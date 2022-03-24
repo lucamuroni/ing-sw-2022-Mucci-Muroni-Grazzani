30528b633c2a513f1025f0672214f86361e07a62
Davide Grazzani <davide.grazzani@mail.polimi.it>
2022-03-24 21:22:28 +0100

    Added class MotherNature

---
+++ src/main/java/it/polimi/ingsw/model/MotherNature.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.debug.Island;
+
+/**
+ * @author Davide Grazzani
+ * Class designated for the rappresentation of Mother Nature
+ */
+public class MotherNature {
+    private Island place;
+
+    public MotherNature(Island initialPlace){
+        this.place = initialPlace;
+    }
+
+    /**
+     * @param newPosition Island on witch Mother Nature will be set on
+     */
+    public void setPlace (Island newPosition){
+        this.place = newPosition;
+    }
+
+    /**
+     * @return the current position of Mother Nature
+     */
+    public Island getPlace () {
+        return this.place;
+    }
+}
