111d232ed98e0fec1daa763b8afec11218204141
Sara <101007706+sassa2000@users.noreply.github.com>
2022-03-25 10:04:16 +0100

    PawnColor class

---
+++ src/main/java/it/polimi/ingsw/PawnColor.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.model;
+
+/**
+ * @author Sara Mucci
+ * Class that represents the color that a pawn (student-professor) can have.
+ */
+
+public enum PawnColor {
+    BLU, ROSA, GIALLO, ROSSO, VERDE;
+}
