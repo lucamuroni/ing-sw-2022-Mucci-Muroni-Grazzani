783ed6119cc9ddbd19f2b4b7631e15bfc1c3677a
Davide Grazzani <davide.grazzani@mail.polimi.it>
2022-03-26 23:23:28 +0100

    new update

--- src/main/java/it/polimi/ingsw/model/Island.java
+++ src/main/java/it/polimi/ingsw/model/Island.java
@@ -1,62 +1,42 @@
-package it.polimi.ingsw;
+package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.debug.Gamer;
 import it.polimi.ingsw.debug.Student;
-import it.polimi.ingsw.debug.StudentsAdder;
-import it.polimi.ingsw.debug.Tower;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
-public class Island implements IslandInterface{
-    private ArrayList<Tower> towers;
-    private ArrayList<StudentsAdder> students;
+public class Island{
+    private int numTowers;
+    private ArrayList<Student> students;
+    private Optional<Gamer> owner;
 
     public Island(){
-        this.towers = new ArrayList<Tower>();
-        students = new ArrayList<StudentsAdder>(5);
-        int i = 0;
-        for (PawnColor color : PawnColor.values()){
-            StudentsAdder student = new StudentsAdder(color);
-            this.students.add(student);
-        }
+        this.numTowers = 0;
+        this.students = new ArrayList<Student>();
+        this.owner.isEmpty();
     }
 
     public void addStudents(Student student){
-        for(StudentsAdder students : this.students){
-            if(students.getColor().equals(student.getColor())){
-                students.addStudent();
-            }
-        }
+        this.students.add(student);
     }
 
     public void mergeIsland(Island island){
-        for(StudentsAdder students : this.students){
-            System.out.println("Prima :"+students.getNumStudents()+"studenti di colore"+students.getColor().toString());
-            for(StudentsAdder otherstudents : island.getStudents()){
-                System.out.println("Studenti da aggiungere :"+otherstudents.getNumStudents()+"studenti di colore"+otherstudents.getColor().toString());
-                if(students.getColor().equals(otherstudents.getColor())){
-                    students.addStudent(otherstudents.getNumStudents());
-                }
-            }
-            System.out.println("Dopo :"+students.getNumStudents()+"studenti di colore"+students.getColor().toString());
-        }
-        System.out.println("torri prima : "+towers.size());
-        this.towers.addAll(island.getTowers());
-        /**
-         * QUESTION
-         * aggiungendo elementi ad un arrayList questi venogono duplicati oppure si passa solo il riferminto?
-         */
-        System.out.println("torri dopo : "+towers.size());
+        this.numTowers += island.getNumTowers();
+        this.students.addAll(island.getStudents());
     }
 
-    public void refreshIslandState(){
-
+    private int getNumTowers(){
+        return this.numTowers;
     }
 
-    private ArrayList<StudentsAdder> getStudents(){
+    private ArrayList<Student> getStudents(){
         return this.students;
     }
 
-    private ArrayList<Tower> getTowers(){
-        return this.towers;
+    public void setOwner(Gamer gamer){
+        this.owner = Optional.of(gamer);
     }
+
+    
 }
