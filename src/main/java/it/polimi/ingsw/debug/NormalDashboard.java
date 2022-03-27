e38fa4225d1abc78d9f429a0d306d02889fdc076
luca <luca.muroni@mail.polimi.it>
2022-03-27 23:35:55 +0200

    Modificata classe NormalDashboard con l'aggiunta di attributi e costruttore

--- src/main/java/it/polimi/ingsw/debug/NormalDashboard.java
+++ src/main/java/it/polimi/ingsw/debug/NormalDashboard.java
@@ -1,4 +1,14 @@
 package it.polimi.ingsw.debug;
 
+import java.util.ArrayList;
+
 public class NormalDashboard {
+    private ArrayList<Student> waitingRoom;
+    private int towers;
+    private ArrayList<Student> hall;
+
+    public NormalDashboard(ArrayList<Student> students, int towers){
+        this.waitingRoom = students;
+        this.towers = towers;
+    }
 }
