02ec70fde083109110422776c15d91bf459f65e2
Sara <101007706+sassa2000@users.noreply.github.com>
2022-03-27 11:19:25 +0200

    Student class

---
+++ src/main/java/it/polimi/ingsw/model/Student.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.model;
+
+/**
+ * @author Sara Mucci
+ * Class that implements a student.
+ */
+
+public class Student extends Pawn{
+    private PawnColor color;
+
+    /**
+     * Class constructor.
+     * @param color -> student color.
+     */
+    public Student(PawnColor color) {
+        super(color);
+    }
+
+    /**
+     * @return the student color.
+     */
+    public PawnColor getColor() {
+        return this.color;
+    }
+}
