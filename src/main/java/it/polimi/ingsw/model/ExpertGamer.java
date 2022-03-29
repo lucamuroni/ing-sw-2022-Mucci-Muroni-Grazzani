148d5fc16b6863540c350b3f2f0f4b6b3297db2a
luca <luca.muroni@mail.polimi.it>
2022-03-29 13:16:38 +0200

    Aggiunta descrizione alla classe ExpertGamer e ai suoi metodi

--- src/main/java/it/polimi/ingsw/model/ExpertGamer.java
+++ src/main/java/it/polimi/ingsw/model/ExpertGamer.java
@@ -7,18 +7,36 @@ import it.polimi.ingsw.debug.Student;
 
 import java.util.ArrayList;
 
+/**
+ * @author luca
+ * Class that represents a player in the expert's game version
+ */
 public class ExpertGamer extends NormalGamer{
     private ExpertDashboard dashboard;
 
+    /**
+     * Class constructor that calls the upper class constructor
+     * @param token represent the unique number associate with a player
+     * @param username represent the name choose by the player that will be displayed
+     */
     public ExpertGamer(int token, String username) {
         super(token, username);
     }
 
+    /**
+     * This override use an expert dashboard instead of a normal one
+     * @param students represents the students taken by the player from the bag at the start of the game
+     * @param towers represents the towers that every player must have (the number of towers depends on how
+     */
     @Override
     public void initGamer(ArrayList<Student> students, int towers) {
         dashboard = new ExpertDashboard(students, towers);
     }
 
+    /**
+     * This method calls the upper class method without modifying it
+     * @param cloud is the cloud choose by the player
+     */
     @Override
     public void selectCloud(Cloud cloud) {
         super.selectCloud(cloud);
