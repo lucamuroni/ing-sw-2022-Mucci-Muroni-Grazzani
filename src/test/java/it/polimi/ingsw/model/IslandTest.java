3f6e56106e073f6348d3724efff5e1304521a254
Davide Grazzani <davide.grazzani@mail.polimi.it>
2022-03-29 16:25:41 +0200

    Creazione classe test per Island

---
+++ src/test/java/it/polimi/ingsw/model/IslandTest.java
@@ -0,0 +1,36 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IslandTest {
+
+    @Test
+    void addStudents() {
+    }
+
+    @Test
+    void mergeIsland() {
+    }
+
+    @Test
+    void getNumTowers() {
+    }
+
+    @Test
+    void setOwner() {
+    }
+
+    @Test
+    void getInfluenceByColor() {
+    }
+
+    @Test
+    void getOwner() {
+    }
+
+    @Test
+    void addTower() {
+    }
+}
