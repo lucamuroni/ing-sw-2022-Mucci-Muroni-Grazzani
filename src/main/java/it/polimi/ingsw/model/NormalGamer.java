8e44785587542a57ff08f089d0a279021e892486
luca <luca.muroni@mail.polimi.it>
2022-03-29 13:03:28 +0200

    Aggiunta descrizione della classe NormalGamr e dei suoi metodi

--- src/main/java/it/polimi/ingsw/model/NormalGamer.java
+++ src/main/java/it/polimi/ingsw/model/NormalGamer.java
@@ -5,6 +5,11 @@ import it.polimi.ingsw.debug.Student;
 
 import java.util.ArrayList;
 
+/**
+ * @author luca
+ * Class that represent a gamer in a non-expert game
+ */
+
 public class NormalGamer {
     private final int token;
     private String username;
@@ -14,6 +19,11 @@ public class NormalGamer {
     private TowerColor playerColor;
     private NormalDashboard dashboard;
 
+    /**
+     * Class constructor
+     * @param token represent the unique number associate with a player
+     * @param username represent the name choose by the player that will be displayed
+     */
     public NormalGamer(int token, String username/*, Stream input*/) {
         this.token = token;
         this.username = username;
@@ -22,10 +32,22 @@ public class NormalGamer {
         deck = new AssistantCardDeck();
     }
 
+    /**
+     * This method is called by the controller when the player he has to choose
+     * a cloud and he has to take its students and put them in his waiting room in the dashboard
+     * @param cloud is the cloud choose by the player
+     */
     public void selectCloud(Cloud cloud){
         dashboard.addStudentsWaitingRoom(cloud.pullStudents());
     }
 
+    /**
+     * This method is called by the controller when all players connect to the game and the game's objects
+     * can be inizialized
+     * @param students represents the students taken by the player from the bag at the start of the game
+     * @param towers represents the towers that every player must have (the number of towers depends on how
+     * many players are playing)
+     */
     public void initGamer(ArrayList<Student> students, int towers){
         dashboard = new NormalDashboard(students, towers);
     }
