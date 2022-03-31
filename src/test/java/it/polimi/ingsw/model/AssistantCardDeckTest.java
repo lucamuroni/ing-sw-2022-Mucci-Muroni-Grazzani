45d6d726ffd578d06c1aec2273ed984a569906a5
luca <luca.muroni@mail.polimi.it>
2022-03-31 17:27:27 +0200

    Implementati tutti i test per AssistantCardDeck tranne per il metodo setCurrentSelection che ha ancora dei problemi

---
+++ src/test/java/it/polimi/ingsw/model/AssistantCardDeckTest.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssistantCardDeckTest {
+
+    AssistantCardDeck deck = new AssistantCardDeck();
+
+    @Test
+    void getCardList() {
+        ArrayList<AssistantCard> cards = deck.getCardList();
+        assertTrue(deck.getCardList().containsAll(cards));
+    }
+
+    @Test
+    void getCurrentSelection() {
+        AssistantCard card = deck.getCurrentSelection();
+        assertEquals(card, deck.getCurrentSelection());
+    }
+
+    @Test
+    void getPastSelection() {
+        AssistantCard card = deck.getPastSelection();
+        assertEquals(card, deck.getPastSelection());
+    }
+
+    @Test
+    void setCurrentSelection() {
+        ArrayList<AssistantCard> cards = deck.getCardList();
+        deck.setCurrentSelection(cards.get(1));
+        assertEquals(2, deck.getCurrentSelection().getTurnValue());
+        assertEquals(1, deck.getCurrentSelection().getMovement());
+
+        // Problema: non funziona l'eliminazione della carta dalla lista del deck
+        //
+        // assertEquals(cards.get(1), deck.getCardList().get(1));
+    }
+}
