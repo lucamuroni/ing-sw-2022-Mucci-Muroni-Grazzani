54da9125ff0309247a69316ed1793e51a45fb74f
luca <luca.muroni@mail.polimi.it>
2022-03-31 16:48:52 +0200

    Modificati metodi setCurrentSelection e setPastSelection

--- src/main/java/it/polimi/ingsw/model/AssistantCardDeck.java
+++ src/main/java/it/polimi/ingsw/model/AssistantCardDeck.java
@@ -1,7 +1,5 @@
 package it.polimi.ingsw.model;
 
-import it.polimi.ingsw.model.AssistantCard;
-
 import java.util.ArrayList;
 
 /**
@@ -15,14 +13,17 @@ public class AssistantCardDeck {
 
     /**
      * Class constructor
+     * It is based on a simple pattern: every time are created two cards, the move increments by 1. This is proved by
+     * the fact that the values for the cards are (turn, move):
+     *  (1,1), (2,1), (3,2), (4,2), (5,3), (6,3), (7,4), (8,4), (9,5), (10,5)
      */
     public AssistantCardDeck(){
-        cardList = new ArrayList<AssistantCard>(10);
-        int temp=1;
-        for(int i=1; i<=10; i++){
-            cardList.add(new AssistantCard(i,temp));
-            if(i%2==0){
-                temp++;
+        cardList = new ArrayList<AssistantCard>();
+        int move=1;
+        for(int turn=1; turn<=10; turn++){
+            cardList.add(new AssistantCard(turn,move));
+            if(turn%2==0){
+                move++;
             }
         }
     }
@@ -52,18 +53,21 @@ public class AssistantCardDeck {
     }
 
     /**
-     * Setter method
+     * Setter method. It also cals the private method setPastSelection to set the currentSelection of the last round
+     * as the new past selection
      * @param currentSelection is the card selected by the player, from his cardList, to play this round
      */
     public void setCurrentSelection(AssistantCard currentSelection) {
+        setPastSelection(this.currentSelection);
         this.currentSelection = currentSelection;
+        cardList.remove(currentSelection);
     }
 
     /**
      * Setter method
      * @param pastSelection is the card selected by the player the last round (previously it was the currentSelection)
      */
-    public void setPastSelection(AssistantCard pastSelection) {
+    private void setPastSelection(AssistantCard pastSelection) {
         this.pastSelection = pastSelection;
     }
 }
