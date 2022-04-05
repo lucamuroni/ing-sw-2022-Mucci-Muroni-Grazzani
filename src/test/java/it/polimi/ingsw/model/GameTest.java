7f39a2df5a55ba6099b77eaa1f4b4a6493e66a8a
luca <luca.muroni@mail.polimi.it>
2022-04-05 14:43:51 +0200

    Aggiunta classe test di Game

---
+++ src/test/java/it/polimi/ingsw/model/GameTest.java
@@ -0,0 +1,72 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void initIsland() {
+    }
+
+    @Test
+    void moveMotherNature() {
+    }
+
+    @Test
+    void getMotherNatureDestination() {
+    }
+
+    @Test
+    void changeProfessorOwner() {
+    }
+
+    @Test
+    void checkIslandOwner() {
+    }
+
+    @Test
+    void updatePlayersOrder() {
+    }
+
+    @Test
+    void getMotherNature() {
+    }
+
+    @Test
+    void getClouds() {
+    }
+
+    @Test
+    void getProfs() {
+    }
+
+    @Test
+    void getIslands() {
+    }
+
+    @Test
+    void getBag() {
+    }
+
+    @Test
+    void getGamers() {
+    }
+
+    @Test
+    void getCurrentPlayer() {
+    }
+
+    @Test
+    void setCurrentPlayer() {
+    }
+
+    @Test
+    void getTurnNumber() {
+    }
+
+    @Test
+    void setTurnNumber() {
+    }
+}
