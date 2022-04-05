0a7a05fdb301a6c06c33801f311b5af340ca5a51
sassa2000 <sara.mucci@mail.polimi.it>
2022-04-05 13:36:35 +0200

    Classe ExpertGamerTest creata

---
+++ src/test/java/it/polimi/ingsw/model/ExpertGamerTest.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpertGamerTest {
+
+    @Test
+    void initGamer() {
+
+    }
+}
