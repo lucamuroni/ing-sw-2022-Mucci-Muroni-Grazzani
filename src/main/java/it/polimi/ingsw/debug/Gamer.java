b0e38c391d103d1057aa76a31b6b9f3b5809fa94
sassa2000 <sara.mucci@mail.polimi.it>
2022-04-10 12:49:41 +0200

    Modificato costruttore Gamer

--- src/main/java/it/polimi/ingsw/debug/Gamer.java
+++ src/main/java/it/polimi/ingsw/debug/Gamer.java
@@ -1,7 +1,16 @@
 package it.polimi.ingsw.debug;
 
 public class Gamer {
+    int token;
+    String username;
     public int getToken() {
         return 123;
+    }
+    public Gamer(int token, String username) {
+        this.token = token;
+        this.username = username;
+    }
+    public Gamer() {
+        super();
     }
 }
