8beb21c7388d88adc6321aeb26368da868c913e3
sassa2000 <sara.mucci@mail.polimi.it>
2022-04-16 09:58:51 +0200

    Created ExpertGameTest class

---
+++ src/test/java/it/polimi/ingsw/model/ExpertGameTest.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.debug.CharacterCard;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpertGameTest {
+
+    @Test
+    void playCard() {
+        CharacterCard card = new CharacterCard();
+
+    }
+}
