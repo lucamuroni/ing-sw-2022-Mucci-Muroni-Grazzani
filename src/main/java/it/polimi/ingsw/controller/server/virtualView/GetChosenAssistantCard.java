f8959d066cac36e6951552bc37d58767c7e0857c
sassa2000 <sara.mucci@mail.polimi.it>
2022-05-10 14:25:19 +0200

    Aggiunto javadoc

--- src/main/java/it/polimi/ingsw/controller/server/virtualView/GetChosenAssistantCard.java
+++ src/main/java/it/polimi/ingsw/controller/server/virtualView/GetChosenAssistantCard.java
@@ -7,20 +7,34 @@ import it.polimi.ingsw.controller.networking.exceptions.ClientDisconnectedExcept
 import it.polimi.ingsw.controller.networking.exceptions.MalformedMessageException;
 import it.polimi.ingsw.controller.networking.exceptions.TimeHasEndedException;
 import it.polimi.ingsw.model.AssistantCard;
-
 import java.util.ArrayList;
-
 import static it.polimi.ingsw.controller.networking.MessageFragment.ASSISTANT_CARD;
 
+/**
+ * @author Luca Muroni
+ * Class that implements the message to get the assistant card chosen by the current player
+ */
 class GetChosenAssistantCard {
     ArrayList<AssistantCard> cards;
     MessageHandler messageHandler;
 
+    /**
+     * Class Constructor
+     * @param cards represents the arraylist of available cards
+     * @param messageHandler represents the messageHandler used for the message
+     */
     public GetChosenAssistantCard(ArrayList<AssistantCard> cards, MessageHandler messageHandler){
         this.cards = cards;
         this.messageHandler = messageHandler;
     }
 
+    /**
+     * method that implements the message exchange
+     * @return the chosen assistant card
+     * @throws MalformedMessageException launched if the message isn't created in the correct way
+     * @throws TimeHasEndedException launched when the available time for the response has ended
+     * @throws ClientDisconnectedException launched if the client disconnects from the game
+     */
     public AssistantCard handle() throws MalformedMessageException, TimeHasEndedException, ClientDisconnectedException {
         ArrayList<Message> messages = new ArrayList<Message>();
         int topicId = this.messageHandler.getNewUniqueTopicID();
--- src/main/java/it/polimi/ingsw/controller/server/virtualView/View.java
+++ src/main/java/it/polimi/ingsw/controller/server/virtualView/View.java
@@ -16,15 +16,91 @@ import java.util.ArrayList;
 
 import java.util.ArrayList;
 
+/**
+ * @author Davide Grazzani
+ * @author Luca Muroni
+ * @author Sara Mucci
+ * Interface with all the messsages method executed by the server
+ */
 public interface View{
+    /**
+     * Method that handles the messages to set a new current player
+     * @param player represents the new current player
+     */
     public void setCurrentPlayer(Player player);
+
+    /**
+     * Methos that handles the messages to update the status of an island
+     * @param island represents the island to update
+     * @throws MalformedMessageException launched if the message isn't created the correct way
+     * @throws FlowErrorException launched when the client sends an unexpected response
+     * @throws TimeHasEndedException launched when the available time for the response ends
+     * @throws ClientDisconnectedException launched if the client disconnects
+     */
     public void updateIslandStatus(Island island) throws MalformedMessageException, FlowErrorException, TimeHasEndedException, ClientDisconnectedException;
+
+    /**
+     * Method that handles the messages to the status of more than one island (EG: when there is an archipelago)
+     * @param islands represents the islands to update
+     * @throws MalformedMessageException launched if the message isn't created the correct way
+     * @throws FlowErrorException launched when the client sends an unexpected response
+     * @throws TimeHasEndedException launched when the available time for the response ends
+     * @throws ClientDisconnectedException launched if the client disconnects
+     */
     public void updateIslandStatus(ArrayList<Island> islands) throws MalformedMessageException, FlowErrorException, TimeHasEndedException, ClientDisconnectedException;
+
+    /**
+     *
+     */
     public void haltOnError();
+
+    /**
+     * Method that handles the messages to update the status of the dashboards
+     * @param gamers represents the players
+     * @throws MalformedMessageException launched if the message isn't created the correct way
+     * @throws TimeHasEndedException launched when the available time for the response ends
+     * @throws ClientDisconnectedException launched if the client disconnects
+     * @throws FlowErrorException launched when the client sends an unexpected response
+     */
     public void updateDashboards(ArrayList<Gamer> gamers) throws MalformedMessageException, TimeHasEndedException, ClientDisconnectedException, FlowErrorException;
+
+    /**
+     * Methos that handles the messages to update the mother nature location
+     * @param island represents the new mother nature location
+     * @throws MalformedMessageException launched if the message isn't created the correct way
+     * @throws TimeHasEndedException launched when the available time for the response ends
+     * @throws ClientDisconnectedException launched if the client disconnects
+     * @throws FlowErrorException launched when the client sends an unexpected response
+     */
     public void updateMotherNaturePlace(Island island) throws MalformedMessageException, TimeHasEndedException, ClientDisconnectedException, FlowErrorException;
+
+    /**
+     * Method that handles the messages to get the assistant card deck the current player chooses
+     * @param cardDeck represents the available decks
+     * @return the chosen deck
+     * @throws MalformedMessageException launched if the message isn't created the correct way
+     * @throws TimeHasEndedException launched when the available time for the response ends
+     * @throws ClientDisconnectedException launched if the client disconnects
+     */
     public AssistantCardDeckFigures getChosenAssistantCardDeck(ArrayList<AssistantCardDeckFigures> cardDeck) throws MalformedMessageException, TimeHasEndedException, ClientDisconnectedException;
+
+    /**
+     * Method that handles the messages to get the assistant card the current player chooses
+     * @param cardsList represents the available cards
+     * @return the chosen card
+     * @throws MalformedMessageException launched if the message isn't created the correct way
+     * @throws TimeHasEndedException launched when the available time for the response ends
+     * @throws ClientDisconnectedException launched if the client disconnects
+     */
     public AssistantCard getChosenAssistantCard(ArrayList<AssistantCard> cardsList) throws MalformedMessageException, TimeHasEndedException, ClientDisconnectedException;
+
+    /**
+     *
+     * @return
+     * @throws MalformedMessageException
+     * @throws TimeHasEndedException
+     * @throws ClientDisconnectedException
+     */
     public PawnColor getMovedStudentColor() throws MalformedMessageException, TimeHasEndedException, ClientDisconnectedException;
     //TODO: modificare il return di getMovedStudentLocation da int a isola(riferimento dell'isola va preso da game)  
     public int getMovedStudentLocation() throws MalformedMessageException, TimeHasEndedException, ClientDisconnectedException;
