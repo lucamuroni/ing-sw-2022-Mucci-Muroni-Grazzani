1b1afbf1035bfbb3bb013aa9a615e4bbcfce2b85
luca <luca.muroni@mail.polimi.it>
2022-05-11 17:52:23 +0200

    Prima implementazione ActionPhase3

--- src/main/java/it/polimi/ingsw/controller/server/game/ActionPhase3.java
+++ src/main/java/it/polimi/ingsw/controller/server/game/ActionPhase3.java
@@ -1,23 +1,101 @@
 package it.polimi.ingsw.controller.server.game;
 
+import it.polimi.ingsw.controller.networking.Player;
+import it.polimi.ingsw.controller.networking.exceptions.ClientDisconnectedException;
+import it.polimi.ingsw.controller.networking.exceptions.FlowErrorException;
+import it.polimi.ingsw.controller.networking.exceptions.MalformedMessageException;
+import it.polimi.ingsw.controller.networking.exceptions.TimeHasEndedException;
+import it.polimi.ingsw.controller.server.game.exceptions.GenericErrorException;
+import it.polimi.ingsw.controller.server.game.exceptions.ModelErrorException;
 import it.polimi.ingsw.controller.server.game.gameController.GameController;
+import it.polimi.ingsw.controller.server.virtualView.View;
+import it.polimi.ingsw.model.Cloud;
 import it.polimi.ingsw.model.game.Game;
+import it.polimi.ingsw.model.gamer.Gamer;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ActionPhase3 implements GamePhase{
     private final Game game;
     private final GameController controller;
+    private Gamer currentPlayer;
+    private final View view;
+
 
     public ActionPhase3(Game game, GameController controller){
         this.game = game;
         this.controller = controller;
+        this.currentPlayer = this.game.getCurrentPlayer();
+        this.view = this.controller.getView();
     }
     @Override
     public void handle() {
+        try {
+            this.view.phaseChanghe("ActionPhase1");
+        } catch () {}
+        try {
+            this.choseCloud(this.controller.getPlayer(this.game.getCurrentPlayer()));
+            ArrayList<Player> players = new ArrayList<>(this.controller.getPlayers());
+            players.remove(this.controller.getPlayer(this.game.getCurrentPlayer()));
+            for (Player pl : players) {
+                this.view.setCurrentPlayer(pl);
+                try {
+                    try {
+                        this.view.updateCloudsStatus(this.game.getClouds());
+                    } catch (MalformedMessageException | TimeHasEndedException | FlowErrorException e) {
+                        this.view.updateDashboards(this.game.getGamers());
+                    }
+                } catch (MalformedMessageException | ClientDisconnectedException | TimeHasEndedException | FlowErrorException e){
+                    this.controller.handlePlayerError(pl);
+                }
+            }
+        } catch (ModelErrorException e) {
+            this.controller.shutdown();
+            e.printStackTrace();
+            return;
+        } catch (GenericErrorException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
 
+
+    private void choseCloud(Player player) throws GenericErrorException, ModelErrorException {
+        this.view.setCurrentPlayer(player);
+        Cloud chosenCloud = null;
+        ArrayList<Cloud> possibleChoices = new ArrayList<Cloud>();
+        for (Cloud cloud : this.game.getClouds()) {
+            if (!cloud.isEmpty()) {
+                possibleChoices.add(cloud);
+            }
+        }
+        try {
+            try {
+                chosenCloud = this.view.getChosenCloud(possibleChoices);
+            } catch (MalformedMessageException e) {
+                chosenCloud = this.view.getChosenCloud(possibleChoices);
+            } catch (TimeHasEndedException e) {
+                chosenCloud = this.getRandomCloud(possibleChoices);
+            }
+        } catch (MalformedMessageException | ClientDisconnectedException e) {
+            this.controller.handlePlayerError(player);
+            throw new GenericErrorException();
+        } catch (TimeHasEndedException e) {
+            chosenCloud = this.getRandomCloud(possibleChoices);
+        }
+        Gamer gamer = player.getGamer(this.game.getGamers());
+        gamer.getDashboard().addStudentsWaitingRoom(chosenCloud.pullStudent());
+    }
+
+    private Cloud getRandomCloud(ArrayList<Cloud> clouds) {
+        Random random = new Random();
+        int rand = random.nextInt(0, clouds.size());
+        return clouds.get(rand);
     }
 
     @Override
     public GamePhase next() {
-        return null;
+        return new PlanningPhase(this.game, this.controller);
     }
 }
