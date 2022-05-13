e2cf219d3f7bfe014935aa33111293a03e117477
luca <luca.muroni@mail.polimi.it>
2022-05-13 18:34:09 +0200

    Modifiche classi Cloud ed Island + javaDoc

--- src/main/java/it/polimi/ingsw/view/asset/game/Cloud.java
+++ src/main/java/it/polimi/ingsw/view/asset/game/Cloud.java
@@ -4,22 +4,52 @@ import it.polimi.ingsw.model.pawn.Student;
 
 import java.util.ArrayList;
 
+/**
+ * This class represents a Cloud
+ */
 public class Cloud {
     private final int id;
     ArrayList<Student> students;
 
+    /**
+     * Constructor of the class
+     * @param id identifies the cloud
+     */
     public Cloud(int id){
         this.id = id;
         this.students = new ArrayList<Student>();
     }
 
+    /**
+     * This method is used to show in client's view the fill of the cloud
+     * @param students
+     */
     public void addStudent(ArrayList<Student> students){
         this.students.addAll(students);
     }
 
+    /**
+     * This method is used in client's view to show the remove of students from the cloud
+     * @return
+     */
     public ArrayList<Student> removeStudents(){
         ArrayList<Student> result = new ArrayList<Student>(this.students);
         this.students.clear();
         return result;
+    }
+
+    /**
+     * This update method is used when the server notifies that the cloud is now empty
+     */
+    public void update() {
+        this.students.clear();
+    }
+
+    /**
+     * This update method is used when the server notifies that the cloud has been filled again
+     * @param students is the array containing the new students that are on the cloud
+     */
+    public void update(ArrayList<Student> students) {
+        this.students.addAll(students);
     }
 }
--- src/main/java/it/polimi/ingsw/view/asset/game/Island.java
+++ src/main/java/it/polimi/ingsw/view/asset/game/Island.java
@@ -5,15 +5,33 @@ import it.polimi.ingsw.model.pawn.Student;
 
 import java.util.ArrayList;
 
+/**
+ * This class represents an Island
+ */
 public class Island {
     private final int id;
     private ArrayList<Student> students;
     private int numTowers;
     private TowerColor towersColor;
 
+    /**
+     * Constructor of the class
+     * @param id
+     */
     public Island(int id){
         this.id = id;
     }
 
-    public void updateIsland()
+    /**
+     * This method updates all the attributes of the island with the information received from the server
+     * @param students is the array containing all the students that are on the island
+     * @param numTowers is the number of towers on the island/group of islands
+     * @param towersColor is the color of the towers, or rather the color of the owner of the island
+     */
+    public void updateIsland(ArrayList<Student> students, int numTowers, TowerColor towersColor) {
+        this.numTowers = numTowers;
+        this.students.clear();
+        this.students.addAll(students);
+        this.towersColor = towersColor;
+    }
 }
