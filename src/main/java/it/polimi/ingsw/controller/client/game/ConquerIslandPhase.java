71450f61d5b9b454ebeb7f1347d6ecb782c59ddf
luca <luca.muroni@mail.polimi.it>
2022-05-16 23:53:05 +0200

    Modifiche classe ConquerIslandPhase

--- src/main/java/it/polimi/ingsw/controller/client/game/ConquerIslandPhase.java
+++ src/main/java/it/polimi/ingsw/controller/client/game/ConquerIslandPhase.java
@@ -1,4 +1,36 @@
 package it.polimi.ingsw.controller.client.game;
 
-public class ConquerIslandPhase {
+import it.polimi.ingsw.controller.client.ClientController;
+import it.polimi.ingsw.controller.client.networkHandler.Network;
+import it.polimi.ingsw.view.ViewHandler;
+import it.polimi.ingsw.view.asset.game.Game;
+
+public class ConquerIslandPhase implements GamePhase{
+    private final Game game;
+    private final Network network;
+    private final ViewHandler view;
+    private final ClientController clientController;
+    private final PhaseName name = PhaseName.CONQUERISLAND_PHASE;
+
+    public ConquerIslandPhase(Game game, ClientController clientController, ViewHandler view) {
+        this.game = game;
+        this.view = view;
+        this.clientController = clientController;
+        this.network = this.clientController.getNetwork();
+    }
+
+    @Override
+    public void handle() {
+
+    }
+
+    @Override
+    public GamePhase next() {
+        return new Idle(this.game, this.clientController, this.view);
+    }
+
+    @Override
+    public PhaseName getNamePhase() {
+        return name;
+    }
 }
