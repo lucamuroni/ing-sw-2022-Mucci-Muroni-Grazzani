7723219e4870f50449bc8574a74ae146a3856668
sassa2000 <sara.mucci@mail.polimi.it>
2022-05-25 15:43:12 +0200

    Inizio prove con javaFX

--- src/main/java/it/polimi/ingsw/view/gui/Prova.java
+++ src/main/java/it/polimi/ingsw/view/gui/Prova.java
@@ -1,24 +1,24 @@
 package it.polimi.ingsw.view.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import java.util.Objects;
 
 public class Prova extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Prova");    //nome dello stage
-        Label label = new Label("Hello");   //testo da visualizzare
-        Scene scene = new Scene(label, 400, 200);   //label = root dello scene graph; 400 = larghezza; 200 = altezza;
-        stage.setScene(scene);  //per settare la scena
-        stage.showAndWait();   //per mostrare lo stage
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/TestScene.fxml")));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
     }
 
     public static void main(String[] args) {
         Application.launch(args);
     }
-
-
 }
