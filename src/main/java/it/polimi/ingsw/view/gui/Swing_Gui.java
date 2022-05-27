0ef7e1472cdd5a14ed76cc91be6305d814812a59
sassa2000 <sara.mucci@mail.polimi.it>
2022-05-27 15:49:31 +0200

    aggiunto costruttore gui

--- src/main/java/it/polimi/ingsw/view/gui/Swing_Gui.java
+++ src/main/java/it/polimi/ingsw/view/gui/Swing_Gui.java
@@ -1,12 +1,26 @@
 package it.polimi.ingsw.view.gui;
 
+import it.polimi.ingsw.controller.client.ClientController;
 import it.polimi.ingsw.model.AssistantCard;
 import it.polimi.ingsw.model.pawn.Student;
 import it.polimi.ingsw.view.ViewHandler;
+import it.polimi.ingsw.view.asset.game.Game;
 import it.polimi.ingsw.view.asset.game.Island;
 import java.util.ArrayList;
 
 public class Swing_Gui implements ViewHandler {
+    Game game;
+    ClientController clientController;
+
+    public Swing_Gui (Game game, ClientController controller) {
+        this.game = game;
+        this.clientController = controller;
+        this.start();
+    }
+
+    private void start() {
+
+    }
     @Override
     public AssistantCard selectCard(ArrayList<AssistantCard> cards) {
         return null;
