16aeacdc4badaccdcb380ce8c7fdcf4d275d5e98
sassa2000 <sara.mucci@mail.polimi.it>
2022-06-29 15:54:58 +0200

    Minor changes

--- src/test/java/it/polimi/ingsw/model/CloudTest.java
+++ src/test/java/it/polimi/ingsw/model/CloudTest.java
@@ -1,23 +1,22 @@
 package it.polimi.ingsw.model;
 
-
 import it.polimi.ingsw.model.pawn.Student;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CloudTest {
     @Test
     void pullStudent(){
         Cloud cloud = new Cloud(1);
         Bag borsa = new Bag();
-        assertEquals(true,cloud.isEmpty());
-        ArrayList<Student> studenti = new ArrayList<Student>();
+        assertTrue(cloud.isEmpty());
         cloud.pushStudents(borsa.pullStudents(3));
-        assertEquals(false, cloud.isEmpty());
-        assertEquals(true, studenti.isEmpty());
-        studenti.addAll(cloud.pullStudent());
-        assertEquals(true, cloud.isEmpty());
+        assertFalse(cloud.isEmpty());
+        ArrayList<Student> studenti = new ArrayList<>(cloud.pullStudent());
+        assertTrue(cloud.isEmpty());
         assertEquals(3, studenti.size());
         System.out.println("Pull completed.");
     }
@@ -25,9 +24,8 @@ public class CloudTest {
     @Test
     void pushStudents() {
         Bag borsa = new Bag();
-        ArrayList<Student> studenti = new ArrayList<Student>();
         Cloud cloud = new Cloud(2);
-        studenti.addAll(borsa.pullStudents(3));
+        ArrayList<Student> studenti = new ArrayList<>(borsa.pullStudents(3));
         assertEquals(3, studenti.size());
         for (Student s: studenti) {
             cloud.pushStudents(studenti);
@@ -38,7 +36,7 @@ public class CloudTest {
     @Test
     void isEmpty() {
         Cloud cloud = new Cloud(1);
-        assertEquals(true, cloud.isEmpty());
+        assertTrue(cloud.isEmpty());
         System.out.println("The cloud is empty.");
     }
 }
