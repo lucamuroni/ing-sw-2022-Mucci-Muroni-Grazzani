e6191bc581b41996b819a106a8c86a0f66b8f092
luca <luca.muroni@mail.polimi.it>
2022-06-30 11:20:53 +0200

    JavaDoc GamePhase

--- src/main/java/it/polimi/ingsw/controller/server/game/GamePhase.java
+++ src/main/java/it/polimi/ingsw/controller/server/game/GamePhase.java
@@ -1,6 +1,18 @@
 package it.polimi.ingsw.controller.server.game;
 
+/**
+ * @author Davide Grazzani
+ * Interface that represents a generic phase of the game
+ */
 public interface GamePhase {
-    public void handle ();
-    public GamePhase next();
+    /**
+     * Method used to handle the phase
+     */
+    void handle ();
+
+    /**
+     * Method used to change to the next phase
+     * @return the next GamePhase
+     */
+    GamePhase next();
 }
