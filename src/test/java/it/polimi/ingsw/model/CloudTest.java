fe6fd16694886f966446ac2d3bbc8faf50d27425
sassa2000 <sara.mucci@mail.polimi.it>
2022-06-30 09:29:21 +0200

    Modificati test di Cloud

--- src/test/java/it/polimi/ingsw/model/CloudTest.java
+++ src/test/java/it/polimi/ingsw/model/CloudTest.java
@@ -2,41 +2,50 @@ package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.model.pawn.Student;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CloudTest {
+    Cloud cloud = new Cloud(1);
+    Bag bag = new Bag();
+
     @Test
     void pullStudent(){
-        Cloud cloud = new Cloud(1);
-        Bag borsa = new Bag();
         assertTrue(cloud.isEmpty());
-        cloud.pushStudents(borsa.pullStudents(3));
+        cloud.pushStudents(bag.pullStudents(3));
         assertFalse(cloud.isEmpty());
-        ArrayList<Student> studenti = new ArrayList<>(cloud.pullStudent());
+        ArrayList<Student> students = new ArrayList<>(cloud.pullStudent());
         assertTrue(cloud.isEmpty());
-        assertEquals(3, studenti.size());
+        assertEquals(3, students.size());
         System.out.println("Pull completed.");
     }
 
     @Test
     void pushStudents() {
-        Bag borsa = new Bag();
-        Cloud cloud = new Cloud(2);
-        ArrayList<Student> studenti = new ArrayList<>(borsa.pullStudents(3));
-        assertEquals(3, studenti.size());
-        for (Student s: studenti) {
-            cloud.pushStudents(studenti);
-        }
+        ArrayList<Student> students = new ArrayList<>(bag.pullStudents(3));
+        assertEquals(3, students.size());
+        cloud.pushStudents(students);
+        assertEquals(3, cloud.pullStudent().size());
         System.out.println("Push completed.");
     }
 
     @Test
     void isEmpty() {
-        Cloud cloud = new Cloud(1);
         assertTrue(cloud.isEmpty());
         System.out.println("The cloud is empty.");
+    }
+
+    @Test
+    void getID() {
+        assertEquals(1, cloud.getID());
+    }
+
+    @Test
+    void getStudents() {
+        cloud.pushStudents(bag.pullStudents(3));
+        assertEquals(3, cloud.getStudents().size());
+        ArrayList<Student> students = new ArrayList<>(cloud.getStudents());
+        assertEquals(3, students.size());
+        assertEquals(students, cloud.getStudents());
     }
 }
