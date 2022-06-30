add8811e562281197dff4c3ea4e9ed374d883983
sassa2000 <sara.mucci@mail.polimi.it>
2022-06-30 12:03:40 +0200

    Modificati test di Gamer

--- src/test/java/it/polimi/ingsw/model/GamerTest.java
+++ src/test/java/it/polimi/ingsw/model/GamerTest.java
@@ -6,30 +6,27 @@ import it.polimi.ingsw.model.pawn.PawnColor;
 import it.polimi.ingsw.model.pawn.Student;
 import it.polimi.ingsw.model.pawn.TowerColor;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class GamerTest {
     Gamer gamer = new Gamer(1, "luca", TowerColor.GREY);
+    Bag bag = new Bag();
+    Cloud cloud = new Cloud(1);
 
     @Test
     public void selectCloud() {
-        Bag bag = new Bag();
-        ArrayList<Student> students = new ArrayList<Student>(bag.pullStudents(1));
+        ArrayList<Student> students = new ArrayList<>(bag.pullStudents(1));
         gamer.initGamer(students, 6);
-        Cloud cloud = new Cloud(1);
-        ArrayList<Student> students1 = new ArrayList<Student>(bag.pullStudents(3));
+        ArrayList<Student> students1 = new ArrayList<>(bag.pullStudents(3));
         cloud.pushStudents(students1);
         gamer.selectCloud(cloud);
         assertTrue(gamer.getDashboard().getWaitingRoom().containsAll(students1));
-
     }
 
     @Test
     void initGamer() {
-        ArrayList<Student> students = new ArrayList<Student>();
+        ArrayList<Student> students = new ArrayList<>();
         Student s0 = new Student(PawnColor.PINK);
         Student s1 = new Student(PawnColor.BLUE);
         Student s2 = new Student(PawnColor.YELLOW);
@@ -41,35 +38,33 @@ class GamerTest {
         gamer.initGamer(students, towers);
         assertEquals(3, gamer.getDashboard().getWaitingRoom().size());
         assertEquals(6, gamer.getDashboard().getNumTowers());
-        assertEquals(false, gamer.getDashboard().getWaitingRoom().contains(s0));
-        assertEquals(true, gamer.getDashboard().getWaitingRoom().containsAll(students));
+        assertFalse(gamer.getDashboard().getWaitingRoom().contains(s0));
+        assertTrue(gamer.getDashboard().getWaitingRoom().containsAll(students));
         assertEquals(students, gamer.getDashboard().getWaitingRoom());
     }
 
     @Test
     void getDeck() {
-        ArrayList<Student> students = new ArrayList<Student>();
+        ArrayList<Student> students = new ArrayList<>();
         students.add(new Student(PawnColor.BLUE));
         gamer.initGamer(students, 3);
         AssistantCardDeck deck = gamer.getDeck();
-        assertEquals(deck, gamer.getDeck());
+        assertNotNull(deck);
     }
 
     @Test
     void getToken() {
-        int token = gamer.getToken();
-        assertEquals(token, gamer.getToken());
+        assertEquals(1, gamer.getToken());
     }
 
     @Test
     void getUsername() {
-        String name = gamer.getUsername();
-        assertEquals(name, gamer.getUsername());
+        assertEquals("luca", gamer.getUsername());
     }
 
     @Test
     void getDashboard() {
-        ArrayList<Student> students = new ArrayList<Student>();
+        ArrayList<Student> students = new ArrayList<>();
         students.add(new Student(PawnColor.BLUE));
         gamer.initGamer(students, 3);
         Dashboard dashboard = gamer.getDashboard();
@@ -77,4 +72,19 @@ class GamerTest {
         assertEquals(dashboard.getWaitingRoom(), gamer.getDashboard().getWaitingRoom());
     }
 
+    @Test
+    void getTowerColor() {
+        assertEquals(TowerColor.GREY, gamer.getTowerColor());
+    }
+
+    @Test
+    void isActive() {
+        assertTrue(gamer.isActive());
+    }
+
+    @Test
+    void setInactivity() {
+        gamer.setInActivity();
+        assertFalse(gamer.isActive());
+    }
 }
