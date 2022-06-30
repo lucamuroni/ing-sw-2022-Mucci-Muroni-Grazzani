2c5f418a9bc94991da5ece0c8cee8681e8315357
sassa2000 <sara.mucci@mail.polimi.it>
2022-06-30 09:52:02 +0200

    Modificati test di island

--- src/test/java/it/polimi/ingsw/model/MotherNatureTest.java
+++ src/test/java/it/polimi/ingsw/model/MotherNatureTest.java
@@ -1,27 +1,23 @@
 package it.polimi.ingsw.model;
 
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MotherNatureTest {
+    Island island1 = new Island(1);
+    Island island2 = new Island(2);
+    MotherNature motherNature = new MotherNature(island1);
 
     @Test
     void setPlace() {
-        Island island1 = new Island(1);
-        Island island2 = new Island(2);
-        MotherNature motherNature = new MotherNature(island1);
-        assertEquals(island1,motherNature.getPlace());
+        assertEquals(island1, motherNature.getPlace());
         motherNature.setPlace(island2);
-        assertNotEquals(island1,motherNature.getPlace());
-        assertEquals(island2,motherNature.getPlace());
+        assertNotEquals(island1, motherNature.getPlace());
+        assertEquals(island2, motherNature.getPlace());
     }
 
     @Test
     void getPlace() {
-        Island island1 = new Island(1);
-        MotherNature motherNature = new MotherNature(island1);
-        assertEquals(island1,motherNature.getPlace());
+        assertEquals(island1, motherNature.getPlace());
     }
 }
